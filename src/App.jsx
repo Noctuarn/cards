0e604564e493493b8f9dadced5b457e15c897b92
Roman Humenyak <romangumenyak2003@gmail.com>
2023-05-14 15:53:44 +0300

    made cards

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,33 @@ import Cards from './components/Cards/Cards';
 function App() {
 
   const url = "https://jsonplaceholder.typicode.com/users";
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     fetch(url)
     .then((response) => response.json())
-    .then(data => console.log(data));
+    .then(data => {
+      setUsers(data);
+    })
+    .catch(err => {
+      console.warn(err)
+      alert("Ooops, there an error for getting data")
+    })
   }, [])
 
 
   return (
     <div className="App">
       <div className="container">
+        {console.log(users)}
         <h1 className='title'>React cards</h1>
         <button className='btn'>Get data</button>
         <section className='cards'>
-            <Cards/>
+            {users.map((el, index) => {
+              return(
+                <Cards key = {index} id = {el.id} name ={el.name} email ={el.email} address = {el.address.street} phone = {el.phone}/>
+              )
+            })}
         </section>
       </div>
     </div>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "./Cards.scss"
 
-const Cards = () => {
+const Cards = (props) => {
+
+    const {id, name, email, address, phone} = props;
+
     return (  
         <div className="card">
-              <h6 className="card__number">№1</h6> 
-              <h2 className="card__name">Sashko</h2>
-              <h3 className="card__email">sashko88005553535@gmail.com</h3>
-              <p className="card__addres">Rosilna street</p>  
-              <p className="card__phone">+4897123712</p>
+              <h6 className="card__number">№{id}</h6> 
+              <h2 className="card__name">{name}</h2>
+              <h3 className="card__email">{email}</h3>
+              <p className="card__addres">{address}</p>  
+              <p className="card__phone">{phone}</p>
         </div>
     );
 }
